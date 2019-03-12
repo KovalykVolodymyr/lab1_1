20596d3b4c1b3adccd0a34889653fc36adca81ce
wsobczyk <wiktoriasobczyk@gmail.com>
2019-03-12 19:17:45 +0100

    bug fix discount

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Discount.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Discount.java
@@ -1,13 +1,23 @@
 package pl.com.bottega.ecommerce.sales.domain.offer;
 
 public class Discount {
-    private String discountCause;
 
-    public String getDiscountCause() {
-        return discountCause;
+    private String cause;
+    private int value;
+
+    public String getCause() {
+        return cause;
     }
 
-    public void setDiscountCause(String discountCause) {
-        this.discountCause = discountCause;
+    public void setCause(String cause) {
+        this.cause = cause;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
     }
 }
