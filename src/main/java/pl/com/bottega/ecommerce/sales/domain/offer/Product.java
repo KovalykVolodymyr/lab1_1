09591ee04629abcb9fa5835c17c21d6a7dd650d0
wsobczyk <wiktoriasobczyk@gmail.com>
2019-03-12 18:36:47 +0100

    1. Added product class

---
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Product.java
@@ -0,0 +1,57 @@
+package pl.com.bottega.ecommerce.sales.domain.offer;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class Product {
+
+    private String id;
+
+    private BigDecimal price;
+
+    private String name;
+
+    private Date snapshotDate;
+
+    private String productType;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getSnapshotDate() {
+        return snapshotDate;
+    }
+
+    public void setSnapshotDate(Date snapshotDate) {
+        this.snapshotDate = snapshotDate;
+    }
+
+    public String getProductType() {
+        return productType;
+    }
+
+    public void setProductType(String productType) {
+        this.productType = productType;
+    }
+}
