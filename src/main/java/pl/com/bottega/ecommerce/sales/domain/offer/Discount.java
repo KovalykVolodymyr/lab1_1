cf8ecbbf6c9fd8803dae594bd8b75a54b5344c1d
wsobczyk <wiktoriasobczyk@gmail.com>
2019-03-12 19:12:08 +0100

    Added class discount

---
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Discount.java
@@ -0,0 +1,13 @@
+package pl.com.bottega.ecommerce.sales.domain.offer;
+
+public class Discount {
+    private String discountCause;
+
+    public String getDiscountCause() {
+        return discountCause;
+    }
+
+    public void setDiscountCause(String discountCause) {
+        this.discountCause = discountCause;
+    }
+}
