9bbe3b04e257d985aa3dafa99c82ca3c45c67fed
Natalia Ciosek <n_ciosek@wp.pl>
2019-03-12 18:40:44 +0100

    Edycja klasy Product

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Product.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Product.java
@@ -1,5 +1,6 @@
 package pl.com.bottega.ecommerce.sales.domain.offer;
 
+import java.math.BigDecimal;
 import java.util.Date;
 import java.util.Objects;
 
@@ -13,11 +14,14 @@ public class Product {
 
     private Date productSnapshotDate;
 
-    public Product(String productId, String productName, String productType, Date productSnapshotDate) {
+    private BigDecimal price;
+
+    public Product(String productId, String productName, String productType, Date productSnapshotDate, BigDecimal price) {
         this.productId = productId;
         this.productName = productName;
         this.productType = productType;
         this.productSnapshotDate = productSnapshotDate;
+        this.price = price;
     }
 
     public String getProductId() { return productId; }
@@ -28,6 +32,8 @@ public class Product {
 
     public String getProductType() { return productType; }
 
+    public BigDecimal getPrice() { return price; }
+
     @Override public boolean equals(Object o) {
         if (this == o)
             return true;
@@ -35,10 +41,11 @@ public class Product {
             return false;
         Product product = (Product) o;
         return Objects.equals(productId, product.productId) && Objects.equals(productName, product.productName) && Objects.equals(
-                productType, product.productType) && Objects.equals(productSnapshotDate, product.productSnapshotDate);
+                productType, product.productType) && Objects.equals(productSnapshotDate, product.productSnapshotDate) && Objects.equals(
+                price, product.price);
     }
 
     @Override public int hashCode() {
-        return Objects.hash(productId, productName, productType, productSnapshotDate);
+        return Objects.hash(productId, productName, productType, productSnapshotDate, price);
     }
 }
